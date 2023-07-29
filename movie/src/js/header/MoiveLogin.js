00a9a85e4552dd7be707ecf734871a2d679045a8
ejdrp1 <ejdrp1@naver.com>
2023-07-29 20:51:19 +0900

    230729 - 임시 저장

---
+++ movie/src/js/header/MoiveLogin.js
@@ -0,0 +1,159 @@
+import React from "react";
+
+import '../../scss/header/MovieLogin.scss';
+
+const MovieLogin = () => {
+
+    return(
+        <>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            {/* 로그인 모달 */}
+            <div id="Header-Login-Wrapper">
+                <div className="header-login-box">
+
+                
+                {/* Logo */}
+                <div className="header-login-logo-box">
+                    <div className="header-login-logo">Logo</div>
+                </div>
+
+                {/* 사용자 정보 입력 창 */}
+                <div className="header-login-user-info-box">
+                    <div className="header-login-user-email-box">
+                        <input 
+                            className="header-login-user-email" 
+                            id="Email" 
+                            placeholder="이메일을 입력해주세요"
+                        />
+                    </div>
+                    <div className="header-login-user-password-box">
+                        <input 
+                            className="header-login-user-password" 
+                            id="PW" 
+                            placeholder="비밀번호를 입력해주세요"
+                        />
+                    </div>
+                </div>
+
+                {/* 로그인 유지 */}
+                <div className="header-login-keep-login-wrapper">
+                    <div className="header-login-keep-login-box">
+                        <input 
+                            className="header-login-keep-login-check-btn"
+                            id="KeepLogin"
+                            type="checkbox"
+                            name="KeepLogin"
+                        ></input>
+                        <label 
+                            for="KeepLogin" 
+                            className="header-login-keep-login-text"
+                        >로그인 유지</label>
+                    </div>
+                </div>
+
+                {/* 로그인 클릭 */}
+                <div className="header-login-login-box">
+                    <button 
+                        className="header-login-login" 
+                        id="Login"
+                        type="button"
+                    >로그인</button>
+                </div>
+
+                {/* 사용자 정보 찾기 */}
+                <div className="header-login-find-info-wrapper">
+
+                    <div className="header-login-find-info-box">
+                        <div className="header-login-find-email-pw-box">
+                            <div className="header-login-find-email">아이디 찾기</div>
+                            <div className="header-login-find-pw">비밀번호 찾기</div>
+                        </div>
+                    
+                        <div className="header-login-signup-box">
+                            <div className="header-login-signup">회원가입</div>
+                        </div>
+                    </div>
+
+                </div>
+
+                {/* 소셜 로그인 */}
+                <div className="header-login-social-login-box">
+                    <div className="header-login-social-login">
+                        <div className="header-login-social-login-img-box">
+                            <div className="header-login-social-login-img" id="Kakao"></div>
+                        </div>
+                        <div className="header-login-social-login-text-box">
+                            <p className="header-login-social-login-select-text">카카오로</p>
+                            <p className="header-login-social-login-text">시작하기</p>
+                        </div>
+                    </div>
+                    <div className="header-login-social-login">
+                        <div className="header-login-social-login-img-box">
+                            <div className="header-login-social-login-img" id="Naver"></div>
+                        </div>
+                        <div className="header-login-social-login-text-box">
+                            <p className="header-login-social-login-select-text">네이버로</p>
+                            <p className="header-login-social-login-text">시작하기</p>
+                        </div>
+                    </div>
+                    <div className="header-login-social-login">
+                        <div className="header-login-social-login-img-box">
+                            <div className="header-login-social-login-img" id="Google"></div>
+                        </div>
+                        <div className="header-login-social-login-text-box">
+                            <p className="header-login-social-login-select-text">구글로</p>
+                            <p className="header-login-social-login-text">시작하기</p>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+
+
+
+                
+                </div>
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </>
+    );
+
+}
+
+export default MovieLogin;
--- movie/src/js/header/MovieHeader.js
+++ movie/src/js/header/MovieHeader.js
@@ -1,34 +1,122 @@
-import React from "react";
+import React, { useState } from "react";
+import MovieLogin from "./MoiveLogin";
 
 import '../../scss/header/MovieHeader.scss';
 
+
 const MovieHeader = () => {
+
+    // 로그인
+    const[openLogin, setOpenLogin] = useState(false);
+
+    const clickLogin = e => {
+        setOpenLogin(true);
+    }
+
+
+
+
+
+
+
+
+
+
 
     return (
         <>
-            <div id="Header-Wrapper">
-                <ul className="header-top">
-                    <li className="ht-social-login">
-                        <div className="social-login-box">
-                            <div className="kakao"></div>
-                            <div className="naver"></div>
-                            <div className="google"></div>
-                        </div>
-                    </li>
-                    <li className="ht-main-logo">
-                        <div className="main-logo"></div>
-                    </li>
-                    <li className="ht-login">
-                        <div className="login"></div>
-                    </li>
-                </ul>
 
-                <div className="header-bottom">
-                    
+            {openLogin && <MovieLogin />}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            <div id="Header-Wrapper">
+
+                <div className="header-top-banner">
+
+                    <div className="header-main-logo-box">
+                        <div className="header-main-logo">메인 로고</div>
+                    </div>
+
+                    <div className="header-menu-box">
+                        <ul className="header-menu">
+                            <button 
+                                className="header-menu-list" 
+                                id="Login"
+                                onClick={clickLogin}
+                            >로그인</button>
+                            <li className="header-menu-list">menu2</li>
+                            <li className="header-menu-list">menu3</li>
+                            <li className="header-menu-list">menu4</li>
+                            <li className="header-menu-list">menu5</li>
+                        </ul>
+                    </div>
+
                 </div>
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                <div className="header-bottom-banner">
+
+                </div>
+
+
+
+
             </div>
+
+
+
+
+
+
+
+
+
+
+
+
 
         </>
     );
