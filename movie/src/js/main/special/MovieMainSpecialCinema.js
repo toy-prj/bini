41f036f3d10001e9517b0adaa678df215ff74660
ejdrp1 <ejdrp1@naver.com>
2023-08-07 01:02:23 +0900

    230807 - main special UI 임시 저장

--- movie/src/js/main/special/MovieMainSpecialCinema.js
+++ movie/src/js/main/special/MovieMainSpecialCinema.js
@@ -1,10 +1,34 @@
 import React from "react";
 
+import '../../../scss/main/special/MovieMainSpecialCinema.scss';
+import { AiOutlineRight } from "react-icons/ai";
+import MovieMainSpecialCinemaMain from "./MovieMainSpecialCinemaMain";
+
 const MovieMainSpecialCinema = () => {
 
     return(
         <>
-            <div>MovieMainSpecialCinema</div>
+            <div className="movie-main-special-cinema-wrapper">
+                <div className="movie-main-special-cinema-box">
+
+                    <div className="movie-main-special-cinema-header-box">
+                        <div className="movie-main-special-cinema-header-title">
+                            <p className="movie-main-special-cinema-header-title-text">스페셜관</p>
+                        </div>
+                        <button className="movie-main-special-cinema-header-view-all">
+                            <p className="movie-main-special-cinema-header-view-all-text">더보기
+                                <p id="viewAll"><AiOutlineRight /></p>
+                            </p>
+                        </button>
+                    </div>
+
+                    <div className="movie-main-special-cinema-main-box">
+                        <MovieMainSpecialCinemaMain />
+                    </div>
+
+
+                </div>
+            </div>
         </>
     );
 }
---
+++ movie/src/js/main/special/MovieMainSpecialCinemaMain.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+import '../../../scss/main/special/MovieMainSpecialCinemaMain.scss';
+
+const MovieMainSpecialCinemaMain = () => {
+
+    return(
+        <>
+            <div className="movie-main-special-cinema-main-wrapper">
+                <div className="movie-main-special-cinema-main-select-box">
+
+                    <div className="movie-main-special-cinema-main-img-box">
+                        <div className="movie-main-special-cinema-main-img"></div>
+                    </div>
+
+                    <div className="movie-main-special-cinema-main-select-menu-box">
+                        <div className="movie-main-special-cinema-main-select-menu">
+
+                            <div className="movie-main-special-cinema-main-select-menu-list">
+                                <div className="movie-main-special-cinema-main-select-menu-list-text-box">
+                                    <p className="movie-main-special-cinema-main-select-menu-list-text">SUITE CINEMA</p>
+                                </div>
+                                <div className="movie-main-special-cinema-main-select-menu-list-tag-box">
+                                    <p className="movie-main-special-cinema-main-select-menu-list-tag">#호텔 컨셉의 프리미엄관</p>
+                                </div>
+                            </div>
+                            <div className="movie-main-special-cinema-main-select-menu-list">
+                                <div className="movie-main-special-cinema-main-select-menu-list-text-box">
+                                    <p className="movie-main-special-cinema-main-select-menu-list-text">CINE & LIVINGROOM</p>
+                                </div>
+                                <div className="movie-main-special-cinema-main-select-menu-list-tag-box">
+                                    <p className="movie-main-special-cinema-main-select-menu-list-tag">#신개념 소셜 상영관</p>
+                                </div>
+                            </div>
+                            <div className="movie-main-special-cinema-main-select-menu-list">
+                                <div className="movie-main-special-cinema-main-select-menu-list-text-box">
+                                    <p className="movie-main-special-cinema-main-select-menu-list-text">4DX</p>
+                                </div>
+                                <div className="movie-main-special-cinema-main-select-menu-list-tag-box">
+                                    <p className="movie-main-special-cinema-main-select-menu-list-tag">#모션시트 #오감체험</p>
+                                </div>
+                            </div>
+                            <div className="movie-main-special-cinema-main-select-menu-list">
+                                <div className="movie-main-special-cinema-main-select-menu-list-text-box">
+                                    <p className="movie-main-special-cinema-main-select-menu-list-text">CINE de CHEF</p>
+                                </div>
+                                <div className="movie-main-special-cinema-main-select-menu-list-tag-box">
+                                    <p className="movie-main-special-cinema-main-select-menu-list-tag">#쉐프가 있는 영화관</p>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default MovieMainSpecialCinemaMain;
